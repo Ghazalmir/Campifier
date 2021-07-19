91360f8cbdfcee131bf3a8ca5adce25c1d4aed61
Ghazal Mir <ghazalmirsayah@gmail.com>
2021-07-19 16:37:06 -0400

    Add scroll down visual cue
    - Add scroll down visual cue to the home page
    - pulses and disappears after exiting view port

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -4,6 +4,7 @@
   let cloud1 = document.querySelector('#cloud1')
   let cloud2 = document.querySelector('#cloud2')
   let registerCard = document.querySelector('#registerPageMarketing')
+  let headerCue = document.querySelector('.header-cue')
 
 
   function inViewPort(el) { // checks to see if the item is in the view port or not
@@ -16,6 +17,7 @@
   }
 
   function moveHeader(){
+    // moving the camper when in view port
     if (inViewPort(camperImage))
     { 
       camperImage.classList.add('move-horizontally');}
@@ -23,6 +25,7 @@
       camperImage.classList.remove('move-horizontally');;
      } 
 
+     // moving the clouds when in view port
      if (inViewPort(registerCard))
     { 
       cloud1.classList.add('cloud1-move');
@@ -32,12 +35,16 @@
       cloud1.classList.remove('cloud1-move');
       cloud2.classList.remove('cloud2-move');
      } 
-
+    
+    // parallax scrolling for header content
     let top = window.pageYOffset;
     headerContent.style.transform = `translateY(-${top/1}px)`
     headerContent.style.opacity = 1 - Math.max(top/(window.innerHeight * 0.2), 0)
-    //--------
-    
+
+    // removing the scroll icon after scrolling down
+    let currentCuePosition = headerContent.getBoundingClientRect().top
+    currentCuePosition < 0 ? headerCue.classList.add('d-none') : headerCue.classList.remove('d-none');
+
     
      
     //------
@@ -45,7 +52,6 @@
   }
   window.requestAnimationFrame(moveHeader)
 
- // window.requestAnimationFrame(moveHeader(imageMask))
 
 //----------
     
